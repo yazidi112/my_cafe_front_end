f45a7240e3ef52a9eb7d4ba26a9ffd2c6f36f891
yazidi112 <yazidi.imran@gmail.com>
2020-08-17 15:40:26 +0100

    Updates

---
+++ precache-manifest.01b0421b38a73ced6da949c3936819cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2045fedd2194f7f4302866bcbfa444",
+    "url": "/my_cafe_front_end/index.html"
+  },
+  {
+    "revision": "79b4b85914cb2f3eabe8",
+    "url": "/my_cafe_front_end/static/css/2.34289635.chunk.css"
+  },
+  {
+    "revision": "79b4b85914cb2f3eabe8",
+    "url": "/my_cafe_front_end/static/js/2.218b03ca.chunk.js"
+  },
+  {
+    "revision": "d2faf7decdbb93c44ee30dee3b2590db",
+    "url": "/my_cafe_front_end/static/js/2.218b03ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484612ed75620b389b79",
+    "url": "/my_cafe_front_end/static/js/main.94389f2e.chunk.js"
+  },
+  {
+    "revision": "f3c86fa7c763fda34768",
+    "url": "/my_cafe_front_end/static/js/runtime-main.c0eb4127.js"
+  }
+]);
